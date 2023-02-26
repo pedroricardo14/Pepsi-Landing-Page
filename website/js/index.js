e21bbe485bb8287efb6f9d6fc6e30b81c01baac3
Pedro Ricardo <pedroricardo1032@gmail.com>
2023-02-26 14:47:56 -0300

    Changing bootstrap icons for images. and settings initial styles for media query.

--- website/js/index.js
+++
@@ -1,7 +0,0 @@
-function imgSlider(anything){
-    document.querySelector('.pepsi').src = anything;
-}
-function changeBgColor(color){
-    const sec = document.querySelector('.sec');
-    sec.style.background = color;
-}
